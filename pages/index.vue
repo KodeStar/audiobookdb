09ca1704dead869012b71050a75254934a07bb48
Kode <kodestar@gmail.com>
2021-12-07 21:45:35 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-300 min-h-screen">
+    something here
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './layouts/**/*.vue',
+    './components/**/*.vue',
+    './pages/**/*.vue'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        gray: {
+          150: '#eeedf2'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
