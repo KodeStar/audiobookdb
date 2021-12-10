9d870d40e8e14f27421d92e715bc2b5260f016c0
Kode <kodestar@gmail.com>
2021-12-10 23:49:27 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
