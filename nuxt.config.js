371945a68c95dac2550f709fcf4883abfa37c82d
Kode <kodestar@gmail.com>
2021-12-10 23:24:28 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/audiobookdb/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
