0a0eac10ae9deb44d9ca4e7b594926055705d5d9
KodeStar <KodeStar@users.noreply.github.com>
2021-12-10 23:30:16 +0000

    deploy: 2ea4c6448e8489d658223300d3bebf81856ecfb1

--- _nuxt/static/1639178734/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Faudiobookdb\u002F_nuxt\u002Fstatic\u002F1639178734",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Faudiobookdb\u002F",assetsPath:"\u002Faudiobookdb\u002F_nuxt\u002F",cdnURL:a}}}}(null));
